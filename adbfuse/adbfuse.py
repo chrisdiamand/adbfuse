ab040755cb52e9b46a530a75bdd0e599f3899eb8
Juan Martín <android@nauj27.com>
2012-02-01 19:49:03 +0100

    license and readme

--- adbfuse/adbfuse.py
+++ adbfuse/adbfuse.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 #
+#    FUSE layer for ADB
 #    Copyright (C) 2012  Juan Martín <android@nauj27.com>
 #
-#    This program can be distributed under the terms of the GNU GPL v3.
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 #    See the file COPYING.
+#    Read the file README.
 #
 #    v1.0
 #
